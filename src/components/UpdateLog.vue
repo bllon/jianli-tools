2d33539f57951426ccd3f56466f0fff401480b7b
xubei <1192475069@qq.com>
2022-09-21 19:12:54 +0800

    测试jenkins第4次

--- src/components/UpdateLog.vue
+++ src/components/UpdateLog.vue
@@ -134,7 +134,7 @@ export default {
           icon: 'el-icon-success'
         },
         {
-          content: '测试jenkins自动部署第三次',
+          content: '测试jenkins自动部署第4次',
           timestamp: '2022-09-21',
           size: 'large',
           type: 'primary',
