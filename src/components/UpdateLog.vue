dbcac537b54fc9e115ea011bac3c8d82e9970934
xubei <1192475069@qq.com>
2022-09-21 19:55:58 +0800

    9

--- src/components/UpdateLog.vue
+++ src/components/UpdateLog.vue
@@ -134,7 +134,7 @@ export default {
           icon: 'el-icon-success'
         },
         {
-          content: '测试jenkins自动部署第8次aa',
+          content: '测试jenkins自动部署第9次aa',
           timestamp: '2022-09-21',
           size: 'large',
           type: 'primary',
